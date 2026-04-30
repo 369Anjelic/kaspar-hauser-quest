73e1b29298efa85663f36a79c9f0afceb9298aa1
Anjel <angelica.gomez@unwritten.studio>
2026-04-30 05:32:07 +0200

    fix: correct paths to serve static files from /public directory

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 const apiKey = process.env.ANTHROPIC_API_KEY;
 const deepgramKey = process.env.DEEPGRAM_API_KEY;
@@ -141,15 +141,15 @@ app.get('/api/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/:file(\\w+\\.html)', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', req.params.file));
+  res.sendFile(path.join(__dirname, '../public', req.params.file));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;
